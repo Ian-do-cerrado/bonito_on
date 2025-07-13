81838d29be3f5edb79427c8a552cce3496277c22
Ian <ian@bon.com.br>
2025-07-12 22:13:50 -0400

    Update contact-modal.tsx

--- components/contact-modal.tsx
+++ components/contact-modal.tsx
@@ -1,4 +1,4 @@
-atualize o código original para que eu não cometa erros: "use client"
+"use client"
 
 import { useState } from "react"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
