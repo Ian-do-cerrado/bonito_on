b9c96fab1f22d0a2248c07da3497c1352f0ccabe
Ian <ian@bon.com.br>
2025-07-12 22:30:02 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,23 +28,23 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QBXNL437DG"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QBXNL437DG');
-            `,
-          }}
-        />
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T5GX4T7X');</script>
+<!-- End Google Tag Manager -->
         <link rel="icon" href="/bonitoon.svg" sizes="any" />
         <link rel="icon" href="/bonitoon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${sora.className} antialiased`} suppressHydrationWarning>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5GX4T7X"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <ScrollToTop />
         <LanguageProvider>
           <ContactModalProvider>
